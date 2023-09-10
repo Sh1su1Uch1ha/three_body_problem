8dae6939191b93b7f8e327a0313b9007d3049084
Sh1su1Uch1ha <qamcintyre@gmail.com>
2023-09-09 18:14:33 -0600

    Update blender_script.py

--- blender_resources/blender_script.py
+++ blender_resources/blender_script.py
@@ -1,21 +1,29 @@
 import bpy
-import csv
+import numpy as np
 
-output_file_path = "path_to_your_repo/three_body_data/euler_output.txt"  # Adjust the path accordingly
+# Clear all existing mesh objects (optional)
+bpy.ops.object.select_all(action='DESELECT')
+bpy.ops.object.select_by_type(type='MESH')
+bpy.ops.object.delete()
 
-positions = []
-with open(output_file_path, 'r') as file:
-    reader = csv.reader(file, delimiter='\t')  # assuming tab-separated values
-    next(reader)  # skip header
-    for row in reader:
-        positions.append([[float(row[1]), float(row[2]), float(row[3])],  # Body 1
-                          [float(row[4]), float(row[5]), float(row[6])],  # Body 2
-                          [float(row[7]), float(row[8]), float(row[9])]])  # Body 3
+# Load data from the txt file
+data = np.loadtxt('/Users/quinnmcintyre/GitHub/three_body_problem/three_body_data/euler_output.txt', skiprows=1)
+timesteps, x1, y1, z1, x2, y2, z2, x3, y3, z3 = data.T
 
-# Assuming three spheres named 'Body1', 'Body2', 'Body3'
-bodies = [bpy.data.objects["Body1"], bpy.data.objects["Body2"], bpy.data.objects["Body3"]]
+# Create or update bodies
+body_names = ['Body1', 'Body2', 'Body3']
+positions = [(x1, y1, z1), (x2, y2, z2), (x3, y3, z3)]
 
-for frame_num, position in enumerate(positions, start=1):
-    for i, body in enumerate(bodies):
-        body.location = position[i]
-        body.keyframe_insert(data_path="location", frame=frame_num)
+for idx, body_name in enumerate(body_names):
+    # If the body already exists, use it; otherwise, create a new one
+    body = bpy.data.objects.get(body_name)
+    if not body:
+        bpy.ops.mesh.primitive_uv_sphere_add(radius=0.05, location=(0, 0, 0))
+        body = bpy.context.active_object
+        body.name = body_name
+    
+    # Now, animate the body based on the data
+    x, y, z = positions[idx]
+    for frame_idx, (x_pos, y_pos, z_pos) in enumerate(zip(x, y, z)):
+        body.location = (x_pos, y_pos, z_pos)
+        body.keyframe_insert(data_path="location", frame=frame_idx)
